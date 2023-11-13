c85a2426d186ce7415d3870d6da31eae49a3cbff
Salman <fcsalmu234@gmail.com>
2023-11-14 00:36:04 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+VERSION="0.0.1"
+DESCRIPTION="DSSS Homework 2"
+
+setup(
+    name = "math_quiz",
+    version = VERSION,
+    author = "Salman Salim",
+    license = "Apache 2.0",
+    long_description = DESCRIPTION,
+    url = "https://github.com/salman-5/dsss-hw-2.git"
+)
